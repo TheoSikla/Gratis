3096821bec5e26f138a1e8216493a04437de9108
Theodoros Siklafidis <theosiklafidis7@yahoo.gr>
2019-06-06 12:03:20 +0300

    Make automated install script

---
+++ install-dependencies.sh
@@ -0,0 +1,9 @@
+apt install -y python3
+apt install -y python3-pip
+apt install -y python3-tk
+apt install -y build-essential libxml2-dev libglpk-dev libgmp3-dev libblas-dev liblapack-dev libarpack2-dev python3-dev
+
+python3 -m pip install virtualenv
+
+python3 -m pip install -r requirements.txt
+
