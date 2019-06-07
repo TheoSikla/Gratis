886ad6259d0a3f93383721583e7ab3cf81ce7115
Theodoros Siklafidis <theosiklafidis7@yahoo.gr>
2019-06-07 16:26:27 +0300

    Improve file output speed

--- Generate/Generate.py
+++ Generate/Generate.py
@@ -8,38 +8,48 @@ class Generate:
     def generate(adjacency_type, graph, thread):
         """ Generates the file that will be used to visualize the graph. """
 
+        file_output = ''
+
         if adjacency_type == "Matrix":
-            with open("Output_Files{}matrix.txt".format(path_escape), "w") as f:
-                for i in range(len(graph.edges)):
-                    if thread.isStopped():
-                        graph.reset_graph()
-                        f.close()
-                        sys.exit(0)
+            
+            for i in range(len(graph.edges)):
+                if thread.isStopped():
+                    graph.reset_graph()
+                    f.close()
+                    sys.exit(0)
 
-                    for j in range(len(graph.edges)):
-                        if graph.edges[i][j] == 1:
-                            f.write('1')
-                        else:
-                            f.write('0')
-                    f.write('\n')
-                f.close()
-
+                for j in range(len(graph.edges)):
+                    if graph.edges[i][j] == 1:
+                        file_output += '1'
+                    else:
+                        file_output += '0'
+                file_output += '\n'
+            
         else:
+
+            for vertex, neighbors in graph.neighbors.items():
+                if thread.isStopped():
+                    graph.reset_graph()
+                    f.close()
+                    sys.exit(0)
+
+                file_output += f"{vertex}:"
+
+                for node in graph.neighbors[vertex]:
+                    file_output += f"{node},"
+                
+                file_output = file_output[:-1]
+                file_output += "\n"
+        
+        if adjacency_type == "Matrix":
+
+            with open("Output_Files{}matrix.txt".format(path_escape), "w") as f:
+                f.write(file_output)
+                f.close()
+        else:
+            
             with open("Output_Files{}list.txt".format(path_escape), "w") as f:
-                for vertex, neighbors in graph.neighbors.items():
-                    if thread.isStopped():
-                        graph.reset_graph()
-                        f.close()
-                        sys.exit(0)
-
-                    f.write("{}:".format(vertex))
-                    for node in graph.neighbors[vertex]:
-                        if node == graph.neighbors[vertex][-1]:
-                            f.write("{}".format(node))
-                        else:
-                            f.write("{},".format(node))
-
-                    f.write('\n')
+                f.write(file_output)
                 f.close()
 
 
