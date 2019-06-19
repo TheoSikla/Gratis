360aa0a8edd1b1f2e63493e1b296594639ec4d51
Theodoros Siklafidis <theosiklafidis7@yahoo.gr>
2019-06-19 23:37:12 +0300

    Add Camel Case to string function

---
+++ Support_Folders/camel_case_spliter.py
@@ -0,0 +1,5 @@
+from re import finditer
+
+def camel_case_split(identifier):
+    matches = finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
+    return ' '.join([m.group(0) for m in matches])
