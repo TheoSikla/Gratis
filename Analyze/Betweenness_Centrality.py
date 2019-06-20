ff0da3047b436b29958cfccb1da612206166b978
Theodoros Siklafidis <theosiklafidis7@yahoo.gr>
2019-06-20 12:33:36 +0300

    F-String Betweenness Centrality

--- Analyze/Betweenness_Centrality.py
+++ Analyze/Betweenness_Centrality.py
@@ -25,11 +25,10 @@ def betweenness_centrality(shortest_paths, num_of_vertices, text_area, thread):
 
                                 if str(i) == node:
                                     path_appearances += 1
-                                    # print("Node {} found in path {} --> {}".format(str(i), value[j],
-                                    #                                                path_appearances))
+                                    # print(f"Node {str(i)} found in path {value[j]} --> {path_appearances}")
                     try:
                         node_betweenness += path_appearances / len(value)
-                        # print("{}: +{}".format(i, path_appearances / len(value)))
+                        # print(f"{i}: +{path_appearances / len(value)}")
 
                     except ZeroDivisionError:
                         node_betweenness += 0
@@ -39,7 +38,7 @@ def betweenness_centrality(shortest_paths, num_of_vertices, text_area, thread):
         nodes_betweenness[i] = node_betweenness
 
     for k, v in nodes_betweenness.items():
-        text_area.insert(END, "\nNode v{} with betweenness centrality: {:.5f}".format(k+1, v))
+        text_area.insert(END, f"\nNode v{k + 1} with betweenness centrality: {v:.5f}")
         text_area.insert(END, "\n")
         text_area.see("end")
         sleep(.01)  # Do not remove this.
